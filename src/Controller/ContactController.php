324f5a471edf272a78ab9d888bce16d0e449db65
Flmbfle <erwantotet@gmail.com>
2024-03-06 16:33:43 +0100

    Fonctionnel

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,27 +10,45 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
 class ContactController extends AbstractController
 {
+    private $csrfTokenManager;
+
+    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()&& $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Vérification du token CSRF
+            $submittedToken = $request->request->get('_token');
+            if (!$this->csrfTokenManager->isTokenValid(new CsrfToken('contact_form', $submittedToken))) {
+                throw $this->createAccessDeniedException('Invalid CSRF token.');
+            }
 
             $message = new Contact();
+            
             $data = $form->getData();
+
             $message = $data;
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_accueil');
         }
 
-
-
-
         return $this->render('contact/index.html.twig', [
-            'form' => $form
+            'form' => $form->createView(),
         ]);
     }
 }
